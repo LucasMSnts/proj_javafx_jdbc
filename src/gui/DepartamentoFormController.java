681dd17203b30e46f08a6a0712dd6bc4f4c2f1d3
Lucas Mauro <lucas_de_mauro@hotmail.com>
2019-09-11 10:53:29 -0300

    ValidacaoException

--- src/gui/DepartamentoFormController.java
+++ src/gui/DepartamentoFormController.java
@@ -3,7 +3,9 @@ package gui;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.Set;
 
 import db.DbException;
 import gui.listerners.AlteracaoDadosListener;
@@ -18,6 +20,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Departamento;
+import model.exceptions.ValidacaoException;
 import model.services.DepartamentoService;
 
 public class DepartamentoFormController implements Initializable{
@@ -68,7 +71,11 @@ public class DepartamentoFormController implements Initializable{
 			servico.saveOrUpdate(entity);
 			notificacaoAlteracaoDadosListeners();
 			Utils.currentStage(evento).close();
-		} catch (DbException e) {
+		} 
+		catch (ValidacaoException e) {
+			setErroMensagens(e.getErros());
+		}
+		catch (DbException e) {
 			Alerts.showAlert("Erro salvando objeto", null, e.getMessage(), AlertType.ERROR);
 		}
 	}
@@ -82,8 +89,18 @@ public class DepartamentoFormController implements Initializable{
 	private Departamento getFormData() {
 		Departamento obj = new Departamento();
 		
+		ValidacaoException excecao = new ValidacaoException("Erro de Validação");
+		
 		obj.setId(Utils.tryParseToInt(txtId.getText()));
+		
+		if (txtNome.getText() == null || txtNome.getText().trim().equals("")) {
+			excecao.addErro("Nome", "Campo não pode estar vazio");
+		}
 		obj.setNome(txtNome.getText());
+		
+		if (excecao.getErros().size() > 0) {
+			throw excecao;
+		}
 		
 		return obj;
 	}
@@ -110,6 +127,13 @@ public class DepartamentoFormController implements Initializable{
 		txtId.setText(String.valueOf(entity.getId()));
 		txtNome.setText(entity.getNome());
 	}
-
+	
+	private void setErroMensagens(Map<String, String> erros) {
+		Set<String> campos = erros.keySet();
+		
+		if (campos.contains("Nome")) {
+			labelErroNome.setText(erros.get("Nome"));
+		}
+	}
 }
 
---
+++ src/model/exceptions/ValidacaoException.java
@@ -0,0 +1,23 @@
+package model.exceptions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ValidacaoException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	private Map<String, String> erros = new HashMap<>();
+	
+	public ValidacaoException(String msg) {
+		super(msg);
+	}	
+	
+	public Map<String, String> getErros() {
+		return erros;
+	}
+	
+	public void addErro(String nomeCampo, String mensagemErro) {
+		erros.put(nomeCampo, mensagemErro);
+	}
+}
